aa002d024302b9dbeaf68ee74ca284ea43fbb4f0
Nurul Huda <huddyhuda@gmail.com>
2016-11-08 08:49:27 +0800

    done

--- config/routes.rb
+++ config/routes.rb
@@ -23,9 +23,6 @@ Rails.application.routes.draw do
 
     get '/recipemaker', to: 'static_pages#recipemaker'
 
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
  root 'application#hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
